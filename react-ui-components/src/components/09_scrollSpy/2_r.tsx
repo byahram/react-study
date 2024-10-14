2525b99aa0ac648fa15be0e354efcfa14dc6bf99
Ahram Kim <45866137+byahram@users.noreply.github.com>
2024-10-14 10:44:57 +0900

    [React / VanillaJS] UI 요소 직접 만들기 Part 2 by 정재남

--- react-ui-components/src/components/09_scrollSpy/2_r.tsx
+++ react-ui-components/src/components/09_scrollSpy/2_r.tsx
@@ -1,9 +1,9 @@
-import { useCallback, useEffect, useRef, useState } from 'react'
-import cx from './cx'
-import data from './data'
-import useIntersectionObserver from '@/hook/useIntersectionObserverV2'
+import { useCallback, useEffect, useRef, useState } from "react";
+import cx from "./cx";
+import data from "./data";
+import useIntersectionObserver from "@/hook/useIntersectionObserverV2";
 
-const HeaderHeight = 60
+const HeaderHeight = 60;
 
 const ListItem = ({
   id,
@@ -11,10 +11,10 @@ const ListItem = ({
   title,
   description,
 }: {
-  id: string
-  index: number
-  title: string
-  description: string
+  id: string;
+  index: number;
+  title: string;
+  description: string;
 }) => {
   return (
     <li id={id} data-index={index}>
@@ -24,76 +24,77 @@ const ListItem = ({
         </strong>
       </p>
       <div>
-        {description.split('\r\n').map((line, i) => (
+        {description.split("\r\n").map((line, i) => (
           <p key={i}>{line}</p>
         ))}
       </div>
     </li>
-  )
-}
+  );
+};
 
 const IOOptions: IntersectionObserverInit = {
   rootMargin: `-${HeaderHeight}px 0% 0% 0%`,
   threshold: [0.5, 1],
-}
+};
 
-type Elem = HTMLElement | null
+type Elem = HTMLElement | null;
 const ScrollSpy2 = () => {
-  const [currentIndex, setCurrentIndex] = useState(0)
-  const navsRef = useRef<Elem[]>([])
-  const itemsRef = useRef<Elem[]>([])
-  const { entries } = useIntersectionObserver(itemsRef, IOOptions)
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const navsRef = useRef<Elem[]>([]);
+  const itemsRef = useRef<Elem[]>([]);
+  const { entries } = useIntersectionObserver(itemsRef, IOOptions);
 
   const setCurrentItem = useCallback((index: number) => {
-    setCurrentIndex(index)
+    setCurrentIndex(index);
     navsRef.current[index]?.scrollIntoView({
-      block: 'nearest',
-      inline: 'center',
-      behavior: 'instant',
-    })
-  }, [])
+      block: "nearest",
+      inline: "center",
+      behavior: "instant",
+    });
+  }, []);
 
   const handleNavClick = useCallback((index: number) => {
-    const scrollTop = document.scrollingElement!.scrollTop
-    const itemY = itemsRef.current[index]?.getBoundingClientRect().top || 0
-    const top = scrollTop + itemY - HeaderHeight
+    const scrollTop = document.scrollingElement!.scrollTop;
+    const itemY = itemsRef.current[index]?.getBoundingClientRect().top || 0;
+    const top = scrollTop + itemY - HeaderHeight;
     window.scrollTo({
       top,
-      behavior: 'smooth',
-    })
-  }, [])
+      behavior: "smooth",
+    });
+  }, []);
 
   useEffect(() => {
-    itemsRef.current = data.map((d, i) => document.getElementById(d.id))
-  }, [])
+    itemsRef.current = data.map((d, i) => document.getElementById(d.id));
+  }, []);
 
   useEffect(() => {
     console.log(
-      ...entries.map(e => ({
+      ...entries.map((e) => ({
         index: (e.target as HTMLElement).dataset.index,
         ...e.boundingClientRect.toJSON(),
-      })),
-    )
-    const entryTops = entries.map(e => e.boundingClientRect.top)
-    const topMin = Math.min(...entryTops)
-    const $target = entries.find(e => e.boundingClientRect.top === topMin)?.target as HTMLElement
-    const index = $target?.dataset.index
-    if (typeof index === 'string') setCurrentItem(+index)
-  }, [entries])
+      }))
+    );
+    const entryTops = entries.map((e) => e.boundingClientRect.top);
+    const topMin = Math.min(...entryTops);
+    const $target = entries.find((e) => e.boundingClientRect.top === topMin)
+      ?.target as HTMLElement;
+    const index = $target?.dataset.index;
+    if (typeof index === "string") setCurrentItem(+index);
+  }, [entries]);
 
   return (
-    <div className={cx('ScrollSpy')}>
-      <header className={cx('floatingHeader')}>
-        <h3 className={cx('title')}>
+    <div className={cx("ScrollSpy")}>
+      <header className={cx("floatingHeader")}>
+        <h3 className={cx("title")}>
           스크롤 스파이 #2. React<sub>IntersectionObserver</sub>
         </h3>
-        <ul className={cx('nav')}>
+        <ul className={cx("nav")}>
           {data.map(({ index, id }) => (
             <li
-              className={cx('navItem', { current: currentIndex === index })}
+              className={cx("navItem", { current: currentIndex === index })}
               key={id}
-              ref={r => {
-                navsRef.current[index] = r
+              ref={(r) => {
+                navsRef.current[index] = r;
               }}
             >
               <button onClick={() => handleNavClick(index)}>{index + 1}</button>
@@ -102,12 +103,12 @@ const ScrollSpy2 = () => {
         </ul>
       </header>
       <ul>
-        {data.map(item => (
+        {data.map((item) => (
           <ListItem {...item} index={item.index} key={item.id} />
         ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default ScrollSpy2
+export default ScrollSpy2;
--- react-ui-components/src/routes.ts
+++ react-ui-components/src/routes.ts
@@ -13,6 +13,23 @@ import ScrollSpy1 from "./components/09_scrollSpy/1_r";
 import ScrollSpy2 from "./components/09_scrollSpy/2_r";
 import ScrollSpy3V from "./components/09_scrollSpy/3_v";
 import ScrollSpy4 from "./components/09_scrollSpy/4_r";
+import Snackbar1 from "./components/10_snackbar/1_r";
+import Snackbar2 from "./components/10_snackbar/2_r";
+import Snackbar3V from "./components/10_snackbar/3_v";
+import Modal1 from "./components/11_modal/1_r";
+import Modal2 from "./components/11_modal/2_r";
+import Modal3V from "./components/11_modal/3_v";
+import Modal4 from "./components/11_modal/4_r";
+import Popover1 from "./components/12_popover/1_r";
+import Popover2 from "./components/12_popover/2_r";
+import Popover3 from "./components/12_popover/3_r";
+import ImageSlide from "./components/13_imageSlide";
+import Carousel from "./components/14_carousel";
+import Gallery1 from "./components/15_gallery/1_r";
+import Gallery2 from "./components/15_gallery/2_r";
+import Dropdowns from "./components/16_dropdown";
+import Autocompletes from "./components/17_autocomplete";
+import DndList from "./components/18_dndList";
 
 export const routePaths = [
   "/",
@@ -35,11 +52,23 @@ export const routePaths = [
   "/scrollSpy/3_v",
   "/scrollSpy/4_r",
   "/snackbar",
+  "/snackbar/1_r",
+  "/snackbar/2_r",
+  "/snackbar/3_v",
   "/modal",
+  "/modal/1_r",
+  "/modal/2_r",
+  "/modal/3_v",
+  "/modal/4_r",
   "/popover",
+  "/popover/1_r",
+  "/popover/2_r",
+  "/popover/3_r",
   "/imageSlide",
   "/carousel",
   "/gallery",
+  "/gallery/1_r",
+  "/gallery/2_r",
   "/selectBox",
   "/autoComplete",
   "/dnd",
@@ -200,57 +229,129 @@ export const routes: Record<ROUTE_PATH, ROUTE> = {
   },
   "/snackbar": {
     key: "/snackbar",
-    link: "/snackbar",
+    link: "/snackbar/1_r",
     name: "10. 스낵바",
-    children: null,
+    children: ["/snackbar/1_r", "/snackbar/2_r", "/snackbar/3_v"],
+  },
+  "/snackbar/1_r": {
+    key: "/snackbar/1_r",
+    link: "/snackbar/1_r",
+    name: "R - Context",
+    children: Snackbar1,
+  },
+  "/snackbar/2_r": {
+    key: "/snackbar/2_r",
+    link: "/snackbar/2_r",
+    name: "R - createPortal",
+    children: Snackbar2,
+  },
+  "/snackbar/3_v": {
+    key: "/snackbar/3_v",
+    link: "/snackbar/3_v",
+    name: "Vanilla",
+    children: Snackbar3V,
   },
   "/modal": {
     key: "/modal",
-    link: "/modal",
+    link: "/modal/1_r",
     name: "11. 모달",
-    children: null,
+    children: ["/modal/1_r", "/modal/2_r", "/modal/3_v", "/modal/4_r"],
+  },
+  "/modal/1_r": {
+    key: "/modal/1_r",
+    link: "/modal/1_r",
+    name: "R - Context",
+    children: Modal1,
+  },
+  "/modal/2_r": {
+    key: "/modal/2_r",
+    link: "/modal/2_r",
+    name: "R - createPortal",
+    children: Modal2,
+  },
+  "/modal/3_v": {
+    key: "/modal/3_v",
+    link: "/modal/3_v",
+    name: "Vanilla",
+    children: Modal3V,
+  },
+  "/modal/4_r": {
+    key: "/modal/4_r",
+    link: "/modal/4_r",
+    name: "R - HTML Dialog",
+    children: Modal4,
   },
   "/popover": {
     key: "/popover",
-    link: "/popover",
+    link: "/popover/1_r",
     name: "12. 팝오버",
-    children: null,
+    children: ["/popover/1_r", "/popover/2_r", "/popover/3_r"],
+  },
+  "/popover/1_r": {
+    key: "/popover/1_r",
+    link: "/popover/1_r",
+    name: "R - Basic",
+    children: Popover1,
+  },
+  "/popover/2_r": {
+    key: "/popover/2_r",
+    link: "/popover/2_r",
+    name: "R - createPortal",
+    children: Popover2,
+  },
+  "/popover/3_r": {
+    key: "/popover/3_r",
+    link: "/popover/3_r",
+    name: "R - HTML Dialog",
+    children: Popover3,
   },
   "/imageSlide": {
     key: "/imageSlide",
     link: "/imageSlide",
     name: "13. 이미지 슬라이드",
-    children: null,
+    children: ImageSlide,
   },
   "/carousel": {
     key: "/carousel",
     link: "/carousel",
     name: "14. 캐러셀",
-    children: null,
+    children: Carousel,
   },
   "/gallery": {
     key: "/gallery",
-    link: "/gallery",
+    link: "/gallery/1_r",
     name: "15. 갤러리",
-    children: null,
+    children: ["/gallery/1_r", "/gallery/2_r"],
+  },
+  "/gallery/1_r": {
+    key: "/gallery/1_r",
+    link: "/gallery/1_r",
+    name: "R - w/Carousel",
+    children: Gallery1,
+  },
+  "/gallery/2_r": {
+    key: "/gallery/2_r",
+    link: "/gallery/2_r",
+    name: "R - Viewer",
+    children: Gallery2,
   },
   "/selectBox": {
     key: "/selectBox",
     link: "/selectBox",
     name: "16. 셀렉트 박스",
-    children: null,
+    children: Dropdowns,
   },
   "/autoComplete": {
     key: "/autoComplete",
     link: "/autoComplete",
     name: "17. 자동 완성",
-    children: null,
+    children: Autocompletes,
   },
   "/dnd": {
     key: "/dnd",
     link: "/dnd",
     name: "18. D&D 리스트",
-    children: null,
+    children: DndList,
   },
 };
 
